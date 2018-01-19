456c38d1de197f616d79be4ad2c53b20f4be8a42
Jiaju Ma <majiaju@hotmail.com>
2018-01-18 20:01:22 -0500

    Back to Home Screen buttons added for Paw and Schedule scenes. Adjusted the size of landing UI. Testing the app on iPhone 7 plus

---
+++ Assets/DataFiles/Scripts/BackHome.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackHome : MonoBehaviour {
+
+	// Use this for initialization
+	public void back () {
+		SceneManager.LoadScene ("GUI");
+	}
+	
+
+}
