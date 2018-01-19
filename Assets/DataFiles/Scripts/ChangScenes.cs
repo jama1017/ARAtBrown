a386bc04bdcea89e6e1acd171b8fd7eb32926872
Jiaju Ma <majiaju@hotmail.com>
2018-01-18 19:18:53 -0500

    In progress trying to solve the scaling issue

---
+++ Assets/DataFiles/Scripts/ChangScenes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangScenes : MonoBehaviour {
+
+	// Use this for initialization
+	public void paw () {
+		SceneManager.LoadScene("PawScene");
+	}
+	
+	// Update is called once per frame
+	public void schedule () {
+		SceneManager.LoadScene ("ScheduleScene");
+	}
+}
